ba6654fea3b37b079498d626785c893183198c6b
Devendra Singh <113914000+Dev7083@users.noreply.github.com>
2024-08-01 00:52:39 +0530

    gamma

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,11 @@
 import { Button } from "../components/Button";
+import { useState } from "react";
 import { arrowRight } from "../assets/icons";
-import { statistics } from "../constants";
-import { bigShoeIm } from "../constants";
+import { shoes, statistics } from "../constants";
+import { bigShoe1 } from "../assets/images";
+import { ShoeCard } from "../components/ShoeCard";
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
   return (
     <section
       id="home"
@@ -20,8 +23,8 @@ const Hero = () => {
           <span className="text-coral-red inline-block mt-3 ">Nike</span> Shoes
         </h1>
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
-          Discover stylish Nike arrivals,quality comfort and innovation for your
-          active life.
+          Discover stylish Nike arrivals, quality comfort and innovation for
+          your active life.
         </p>
         <Button label="Shop now" iconURL={arrowRight} />
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16 ">
@@ -35,8 +38,25 @@ const Hero = () => {
           ))}
         </div>
       </div>
-      <div>
-        <img src={bigShoeImage} alt="" />
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img
+          src={bigShoeImg}
+          alt="Shoe Collection"
+          width={610}
+          height={540}
+          className="object-contain relative z-10"
+        />
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard
+                imgURL={shoe}
+                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
